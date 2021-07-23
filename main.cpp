b77d49aa4af4a155711526fd8f5683cc69e7f0bb
user <goobncl@gmail.com>
2021-07-23 14:19:25 +0400

    first commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
 using namespace std;
 
+const int NUM_LINES = 4;
+const int NUM_COLUMNS = 3;
+const int COLUMN_WIDTH = 20;
+
+void PrintTableHeader();
+void PrintTableBody();
+
 int main() {
-    cout << "Hello" << endl;
+    PrintTableHeader();
+    PrintTableBody();
     return 0;
+}
+
+void PrintTableBody() {
+    ifstream input("./data/table-data.txt");
+    for (int k = 0; k < NUM_LINES; ++k) {
+        int intValue;
+        double doubleValue;
+        input >> intValue >> doubleValue;
+        cout << setw(COLUMN_WIDTH) << (k+1) << " | ";
+        cout << setw(COLUMN_WIDTH) << intValue << "|";
+        cout << setw(COLUMN_WIDTH) << doubleValue << endl;
+    }
 }
