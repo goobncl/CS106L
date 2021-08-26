6560ad5639d577513d8b0115c1a383e8eacda97f
user <goobncl@gmail.com>
2021-08-26 22:07:11 +0400

    Condense while condition

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    cout << "ABC" << endl;
+    ifstream capitals("./data/world-capitals.txt");
+    if (!capitals.is_open()) {
+        cerr << "Cannot find the file world-capitals.txt" << endl;
+        return -1;
+    }
+    string capital, country;
+    while (getline(capitals, capital) && getline(capitals, country)) {
+        cout << capital << " is the capital of " << country << endl;
+    }
     return 0;
 }
